e199d12e1ea073f39d80fcdef64d3b3990de6bc4
Nidhi <nidhi19122006@gmail.com>
2026-09-01 14:06:52 +0530

    Time: 9 ms (46.39%) | Memory: 46.9 MB (74.60%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,13 +1,41 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // HashMap <Integer, Integer> map = new HashMap<>();
+        // int ans = 0;
+        // for(int i=0; i<arr.length; i++){
+        //     ans = target-arr[i];
+        //         if(map.containsKey(ans)){
+        //             return new int[]{map.get(ans),i};
+        //         }
+        //           map.put(arr[i], i);
+        //     }
+        //     return new int[]{};
 
-      for(int i=0; i<nums.length; i++){
-      for(int j=i+1; j<nums.length; j++){
-        if(nums[i]+nums[j] == target){
-            return new int[] {i, j};
+        //O(nlogn)
+        //convert 1d array to 2d array
+        int n = nums.length;
+        int[][] arr = new int[n][2];
+       
+        for(int k=0; k<n; k++){
+            // arr.sort((a,b)=>Integer.Compare(a.get(0),b.get(0)));
+            arr[k][0] = nums[k];
+            arr[k][1] = k;
+            
         }
-      }
-      }  
-      return new int[]{};
+         Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+        int i=0;
+        int j = n-1;
+        while(i<j){
+            int sum = arr[i][0]+arr[j][0];
+             if(sum==target){
+                    return new int[]{arr[i][1], arr[j][1]};
+                }else if(sum<target){
+                    i++;
+                }else{
+                    j--;
+                }
+            }
+        return new int[]{};
+        }
+               
     }
-}
